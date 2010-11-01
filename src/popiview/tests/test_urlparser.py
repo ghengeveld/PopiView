3bf22b823a6c65fd8315527c8a8c626f7b54978b
GHengeveld <info@ghengeveld.nl>
2010-11-01 17:56:05 +0000

    Add urlparser

---
+++ src/popiview/tests/test_urlparser.py
@@ -0,0 +1,170 @@
+# coding=utf-8
+
+import urlparse
+import unittest
+from popiview.urlparser import URLParser
+from popiview.tests.test_base import TestBase
+
+class TestURLParser(TestBase):
+
+    def test_searchquery(self):
+        """Test the detection of search query in referrer"""
+        tests = []
+        # Google query
+        tests.append({
+            'ref': u'http://google.nl?q=my query',
+            'expect': (u'google.nl', u'my query')
+        })
+        # Yahoo query
+        tests.append({
+            'ref': u'http://yahoo.com?p=my query',
+            'expect': (u'yahoo.com', u'my query')
+        })
+        # Query with extra querystring parameter before
+        tests.append({
+            'ref': u'http://google.nl/search?sourceid=chrome&q=my query',
+            'expect': (u'google.nl', u'my query')
+        })
+        # Query with single quotes
+        tests.append({
+            'ref': u"http://google.nl?q='my query'",
+            'expect': (u'google.nl', u"'my query'")
+        })
+        # Query with double quotes
+        tests.append({
+            'ref': u'http://google.nl?q="my query"',
+            'expect': (u'google.nl', u'"my query"')
+        })
+        # Query with plus sign instead of space
+        tests.append({
+            'ref': u'http://google.nl?q=my+query',
+            'expect': (u'google.nl', u'my query')
+        })
+        # Cyrillic characters
+        tests.append({
+            'ref': u'http://google.ru?q=русск альф',
+            'expect': (u'google.ru', u'русск альф')
+        })
+        # No referrer
+        tests.append({
+            'ref': None,
+            'expect': None 
+        })
+        # Not a search engine
+        tests.append({
+            'ref': u'http://www.example.com',
+            'expect': None
+        })
+        # No query string
+        tests.append({
+            'ref': u'http://www.google.com',
+            'expect': None
+        })
+        # Empty query string
+        tests.append({
+            'ref': u'http://www.google.com?q=',
+            'expect': None
+        })
+        
+        for test in tests:
+            self.urlparser = URLParser(self._conf)
+            ref = test['ref']
+            if test['ref'] is not None:
+                ref = list(urlparse.urlsplit(ref))
+            self.assertEqual(self.urlparser.searchquery(ref), test['expect'])
+
+    def test_keywords(self):
+        """Test keyword separation from searchquery, converted to lowercase"""
+        tests = []
+        # Regular search
+        tests.append({
+            'query': 'Test SearchQuery',
+            'expect': ['test', u'searchquery']
+        })
+        # Number
+        tests.append({
+            'query': '123',
+            'expect': ['123']
+        })
+        # Cyrillic characters
+        tests.append({
+            'query': u'русск альф',
+            'expect': [u'русск', u'альф']
+        })
+        # Single quotes
+        tests.append({
+            'query': "my 'test query'",
+            'expect': [u'my', u'test query']
+        })
+        # Double quotes
+        tests.append({
+            'query': 'my "test query"',
+            'expect': [u'my', u'test query']
+        })
+
+        for test in tests:
+            self.urlparser = URLParser(self._conf)
+            ref = list(urlparse.urlsplit(u'http://google.com?q='+test['query']))
+            self.assertEqual(self.urlparser.keywords(ref), test['expect'])
+
+    def test_source(self):
+        """Test source (referrer) type detection"""
+        tests = []
+        """# No referrer
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': None,
+            'expect': 'direct'
+        })
+        # Same domain
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': u'http://mysite.com/anotherpage',
+            'expect': 'internal'
+        })
+        # Another domain
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': u'http://anothersite.com/page',
+            'expect': u'external: anothersite.com'
+        })"""
+        # Same domain, different subdomain
+        tests.append({
+            'url': u'http://www.mysite.com/page',
+            'ref': u'http://test.mysite.com/page',
+            'expect': 'external: test.mysite.com'
+        })
+        # Search engine
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': u'http://google.com?q=my "search query"',
+            'expect': 'searches - google.com: my "search query"'
+        })
+        # Search engine, no query
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': u'http://google.com',
+            'expect': u'external: google.com'
+        })
+        # Search engine, empty query
+        tests.append({
+            'url': u'http://mysite.com/page',
+            'ref': u'http://google.com?q=',
+            'expect': u'external: google.com'
+        })
+        
+        for test in tests:
+            self.urlparser = URLParser(self._conf)
+            url = test['url']
+            if url is not None:
+                url = list(urlparse.urlsplit(url))
+            ref = test['ref']
+            if ref is not None:
+                ref = list(urlparse.urlsplit(ref))
+            self.assertEqual(self.urlparser.source(url, ref), test['expect'])
+ 
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestURLParser))
+    return suite
---
+++ src/popiview/urlparser.py
@@ -0,0 +1,71 @@
+import urlparse
+import re
+
+class URLParser(object):
+
+    def __init__(self, config):
+        self._conf = config
+
+    def searchquery(self, ref):
+        sites = self._conf['sparams']
+        if ref is not None:
+            for domain, q in sites.iteritems():
+                domainpos = ref[1].find(domain)
+                if domainpos > -1:
+                    qs = urlparse.parse_qs(ref[3])
+                    query = qs.get(q, None)
+                    if query:
+                        return (ref[1][domainpos:], query[0])
+        return None
+
+    def keywords(self, ref):
+        keywords = []
+        query = self.searchquery(ref)
+        if query is not None:
+            phrase = query[1].lower()
+            pattern = re.compile(r'".*?"|\'.*\'|[^\s]+')
+            for match in re.finditer(pattern, phrase):
+                item = match.group(0)
+                if item.startswith('"') or item.startswith("'"):
+                    item = item[1:-1]
+                keywords.append(item)
+        return keywords
+
+    def source(self, url, ref):
+        if ref is None or ref[1] == '':
+            return 'direct'
+        if url[1] == ref[1]:
+            return 'internal'
+        query = self.searchquery(ref)
+        if query is not None:
+            return 'searches - ' + query[0] + ': ' + query[1]
+        return 'external: ' + ref[1]
+
+    def urlfilter(self, url):
+        # Filters is a list of dictionaries
+        filters = [
+            {'type': 'endswith', 'urlpart': 2, 'find': '/', 'replace': ''},
+            #{'type': 'replace', 'urlpart': 1, 'find': 'www.', 'replace': '',
+            #    'limit': 1},
+            {'type': 'cutoff', 'urlpart': 3, 'find': 'PHPSESSID'}]
+
+        for f in filters:
+            if f['type'] == 'endswith':
+                if url[f['urlpart']].endswith(f['find']):
+                    url[f['urlpart']] = url[f['urlpart']][:-len(f['find'])] + \
+                        f['replace']
+            elif f['type'] == 'replace':
+                if f['limit']:
+                    url[f['urlpart']] = url[f['urlpart']].replace(f['find'], 
+                        f['replace'], f['limit'])
+                else:
+                    url[f['urlpart']] = url[f['urlpart']].replace(f['find'],
+                        f['replace'])
+            elif f['type'] == 'cutoff':
+                strpos = url[f['urlpart']].find(f['find'])
+                if strpos >= 0:
+                    print url[f['urlpart']] + ' (' + str(strpos) + ')'
+                    url[f['urlpart']] = url[f['urlpart']][:strpos]
+            if url[3].endswith('&'):
+                url[3] = url[3][:-1]
+        return url
