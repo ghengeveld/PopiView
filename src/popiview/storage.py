08494a23ea741a047cea1f6bf415bd4a4cd168a7
GHengeveld <info@ghengeveld.nl>
2010-11-05 15:32:28 +0000

    Make recenthits size configurable

--- src/popiview/storage.py
+++ src/popiview/storage.py
@@ -31,8 +31,9 @@ class MemoryStorage(object):
             return
         self._hits.append(hitobj)
         self._recenthits.append(hitobj)
-        if len(self._recenthits) > 500:
-            self._recenthits = self._recenthits[-500:]
+        recenthits_size = int(self._conf['recenthits_size'])
+        if len(self._recenthits) > recenthits_size:
+            self._recenthits = self._recenthits[-recenthits_size:]
 
     def get_recenthits(self, sources, last_timestamp=0):
         recenthits = self._recenthits
@@ -292,8 +293,9 @@ class SQLStorage(object):
         hitobj = {'url': url, 'timestamp': timestamp, 'title': title,
                   'keywords': keywords, 'path': path, 'source': source}
         self._recenthits.append(hitobj)
-        if len(self._recenthits) > 500:
-            self._recenthits = self._recenthits[-500:]
+        recenthits_size = int(self._conf['recenthits_size'])
+        if len(self._recenthits) > recenthits_size:
+            self._recenthits = self._recenthits[-recenthits_size:]
 
     def get_recenthits(self, sources, last_timestamp=0):
         recenthits = self._recenthits
